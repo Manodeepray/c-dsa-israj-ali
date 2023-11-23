716bf8150e809cf609b50a49334859ff3e4969e7
Manodeepray <127145195+Manodeepray@users.noreply.github.com>
2023-11-23 19:59:09 +0530

    23/11

---
+++ DSA_LAB/week10/la1.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+
+// Maximum number of nodes in the graph
+#define MAX_NODES 100
+
+// Function to initialize the graph with all edges set to 0
+void initializeGraph(int graph[MAX_NODES][MAX_NODES], int numNodes)
+{
+    for (int i = 0; i < numNodes; i++)
+    {
+        for (int j = 0; j < numNodes; j++)
+        {
+            graph[i][j] = 0;
+        }
+    }
+}
+
+// Function to add an edge to the graph
+void addEdge(int graph[MAX_NODES][MAX_NODES], int start, int end)
+{
+    graph[start][end] = 1;
+    graph[end][start] = 1; // Assuming an undirected graph
+}
+
+// Function to delete an edge from the graph
+void deleteEdge(int graph[MAX_NODES][MAX_NODES], int start, int end)
+{
+    graph[start][end] = 0;
+    graph[end][start] = 0;
+}
+
+// Function to add a new node to the graph
+void addNode(int graph[MAX_NODES][MAX_NODES], int *numNodes)
+{
+    if (*numNodes < MAX_NODES)
+    {
+        (*numNodes)++;
+    }
+    else
+    {
+        printf("Cannot add more nodes. Maximum limit reached.\n");
+        return;
+    }
+}
+
+// Function to delete a node from the graph
+void deleteNode(int graph[MAX_NODES][MAX_NODES], int *numNodes, int nodeToDelete)
+{
+    if (nodeToDelete >= *numNodes)
+    {
+        printf("Invalid node index.\n");
+        return;
+    }
+
+    for (int i = nodeToDelete; i < *numNodes - 1; i++)
+    {
+        for (int j = 0; j < *numNodes; j++)
+        {
+            graph[i][j] = graph[i + 1][j];
+            graph[j][i] = graph[j][i + 1];
+        }
+    }
+
+    for (int i = 0; i < *numNodes; i++)
+    {
+        graph[*numNodes - 1][i] = 0;
+        graph[i][*numNodes - 1] = 0;
+    }
+
+    (*numNodes)--;
+}
+
+// Function to count the number of nodes and edges in the graph
+void countNodesAndEdges(int numNodes, int graph[MAX_NODES][MAX_NODES])
+{
+    int numEdges = 0;
+
+    for (int i = 0; i < numNodes; i++)
+    {
+        for (int j = 0; j < numNodes; j++)
+        {
+            if (graph[i][j] == 1)
+            {
+                numEdges++;
+            }
+        }
+    }
+
+    printf("Number of nodes: %d\n", numNodes);
+    printf("Number of edges: %d\n", numEdges);
+}
+
+// Function for Depth First Search (DFS)
+void DFS(int node, int numNodes, int graph[MAX_NODES][MAX_NODES], int visited[MAX_NODES])
+{
+    printf("%d ", node);
+    visited[node] = 1;
+
+    for (int i = 0; i < numNodes; i++)
+    {
+        if (graph[node][i] == 1 && !visited[i])
+        {
+            DFS(i, numNodes, graph, visited);
+        }
+    }
+}
+
+// Function for Breadth First Search (BFS)
+void BFS(int start, int numNodes, int graph[MAX_NODES][MAX_NODES], int visited[MAX_NODES])
+{
+    int queue[MAX_NODES];
+    int front = 0, rear = 0;
+
+    printf("%d ", start);
+    visited[start] = 1;
+    queue[rear++] = start;
+
+    while (front < rear)
+    {
+        int currentNode = queue[front++];
+
+        for (int i = 0; i < numNodes; i++)
+        {
+            if (graph[currentNode][i] == 1 && !visited[i])
+            {
+                printf("%d ", i);
+                visited[i] = 1;
+                queue[rear++] = i;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int graph[MAX_NODES][MAX_NODES];
+    int numNodes = 0;
+    int visited[MAX_NODES];
+
+    initializeGraph(graph, MAX_NODES);
+
+    int choice, start, end, nodeToDelete;
+
+    do
+    {
+        printf("\n1. Add Edge\n");
+        printf("2. Delete Edge\n");
+        printf("3. Add Node\n");
+        printf("4. Delete Node\n");
+        printf("5. Count Nodes and Edges\n");
+        printf("6. Depth First Search (DFS)\n");
+        printf("7. Breadth First Search (BFS)\n");
+        printf("8. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter start and end nodes for the new edge: ");
+            scanf("%d %d", &start, &end);
+            addEdge(graph, start, end);
+            break;
+
+        case 2:
+            printf("Enter start and end nodes for the edge to delete: ");
+            scanf("%d %d", &start, &end);
+            deleteEdge(graph, start, end);
+            break;
+
+        case 3:
+            addNode(graph, &numNodes);
+            break;
+
+        case 4:
+            printf("Enter the index of the node to delete: ");
+            scanf("%d", &nodeToDelete);
+            deleteNode(graph, &numNodes, nodeToDelete);
+            break;
+
+        case 5:
+            countNodesAndEdges(numNodes, graph);
+            break;
+
+        case 6:
+            printf("Enter the starting node for DFS: ");
+            scanf("%d", &start);
+            initializeGraph(visited, MAX_NODES);
+            printf("DFS traversal starting from node %d: ", start);
+            DFS(start, numNodes, graph, visited);
+            printf("\n");
+            break;
+
+        case 7:
+            printf("Enter the starting node for BFS: ");
+            scanf("%d", &start);
+            initializeGraph(visited, MAX_NODES);
+            printf("BFS traversal starting from node %d: ", start);
+            BFS(start, numNodes, graph, visited);
+            printf("\n");
+            break;
+
+        case 8:
+            printf("Exiting the program.\n");
+            break;
+
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
+        }
+
+    } while (choice != 8);
+
+    return 0;
+}
---
+++ DSA_LAB/week10/la3.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+// Structure to represent a key-value pair
+struct KeyValue
+{
+    int key;
+    int value;
+};
+
+// Structure to represent a hash table
+struct HashTable
+{
+    struct KeyValue *array;
+    int size;
+};
+
+// Function to initialize a hash table
+struct HashTable *initHashTable(int size)
+{
+    struct HashTable *table = (struct HashTable *)malloc(sizeof(struct HashTable));
+    table->array = (struct KeyValue *)malloc(size * sizeof(struct KeyValue));
+    table->size = size;
+
+    // Initialize all keys to -1 (indicating empty slot)
+    for (int i = 0; i < size; i++)
+    {
+        table->array[i].key = -1;
+    }
+
+    return table;
+}
+
+// Function to calculate the hash value
+int hashCode(int key, int size)
+{
+    return key % size;
+}
+
+// Function to insert a key-value pair into the hash table using linear probing
+void insert(struct HashTable *table, int key, int value)
+{
+    int index = hashCode(key, table->size);
+
+    while (table->array[index].key != -1)
+    {
+        // Linear probing: move to the next slot if the current slot is occupied
+        index = (index + 1) % table->size;
+    }
+
+    // Insert the key-value pair into the found slot
+    table->array[index].key = key;
+    table->array[index].value = value;
+}
+
+// Function to search for a key in the hash table
+int search(struct HashTable *table, int key)
+{
+    int index = hashCode(key, table->size);
+    int searches = 1;
+
+    while (table->array[index].key != key)
+    {
+        // Linear probing: move to the next slot if the current slot is not the target key
+        index = (index + 1) % table->size;
+        searches++;
+
+        // If an empty slot is encountered, the key is not present in the table
+        if (table->array[index].key == -1)
+        {
+            printf("Key %d not found in the hash table.\n", key);
+            return searches;
+        }
+    }
+
+    // Key found in the table
+    printf("Key %d found in the hash table at index %d.\n", key, index);
+    return searches;
+}
+
+// Function to display the content of the hash table
+void display(struct HashTable *table)
+{
+    printf("Hash Table:\n");
+    for (int i = 0; i < table->size; i++)
+    {
+        if (table->array[i].key != -1)
+        {
+            printf("Index %d: Key %d, Value %d\n", i, table->array[i].key, table->array[i].value);
+        }
+    }
+}
+
+int main()
+{
+    struct HashTable *table = initHashTable(SIZE);
+
+    insert(table, 1, 10);
+    insert(table, 2, 20);
+    insert(table, 3, 30);
+
+    display(table);
+
+    int keyToSearch;
+    printf("\nEnter key to search: ");
+    scanf("%d", &keyToSearch);
+
+    int searches = search(table, keyToSearch);
+    printf("Number of searches: %d\n", searches);
+
+    free(table->array);
+    free(table);
+
+    return 0;
+}
